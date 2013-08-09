861c052de175766186f628f091d31a176806780f
GerganaPetrova <josephin.bella@gmail.com>
2013-08-09 17:02:19 +0300

    add new branch with class views

--- books/views.py
+++ books/views.py
@@ -2,63 +2,56 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from books.models import *
 from django.template import RequestContext, loader
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth import logout,login, authenticate
-from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 import random
 import json
+from django.views.generic.base import View
 
-def index(request):
-    try:
-        quotes = Quote.objects.all()
-        quote = random.choice(quotes)
-    except:
-        user = User()
-        book = Book(user = user, title = 'The perks of being walflower', author = 'Steven Chobsky')
-        quotes = [Quote(book = book, user = user, text = 'Right now we are alive and in this moment I swear we are infinite.')]
-        quote = random.choice(quotes)
-    finally:
-        temp = loader.get_template('index.html')
-        context = RequestContext(request, {
-            'quote': quote,
-            'session': request.session,
-        })
-        return HttpResponse(temp.render(context))
+class Index(View):
+    template_name = 'index.html'
 
-def my_login(request):
-    if request.method == 'GET':
-        temp = loader.get_template('login.html')
-        context = RequestContext(request, {
-                'session': request.session,
-        })
-        return HttpResponse(temp.render(context))
-    elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username = username, password = password)
+    def get(self, request):
+        try:
+            quotes = Quote.objects.all()
+            quote = random.choice(quotes)
+        except:
+            user = User()
+            book = Book(user = user, title = 'The perks of being walflower', author = 'Steven Chobsky')
+            quotes = [Quote(book = book, user = user, text = 'Right now we are alive and in this moment I swear we are infinite.')]
+            quote = random.choice(quotes)
+        finally:
+            return render(request, self.template_name, {'quote': quote})
+
+class MyLogin(View):
+    template_name = 'login.html'
+
+    def get(self,request):
+        return render(request, self.template_name)        
+
+    def post(self, request, *args, **kwargs):
+        user = authenticate(username = request.POST['username'], password = request.POST['password'])
         if user is not None:
             login(request, user)
-            temp = loader.get_template('index.html')
-            context = RequestContext(request, {
-                    'user': user,
-            })
-            return HttpResponse(temp.render(context))
+            return HttpResponseRedirect('/')
         else:
-            return HttpResponseRedirect('/login')    
+            return HttpResponseRedirect('/login')
 
-def my_logout(request):
-    logout(request)
-    return HttpResponseRedirect('/')
+class MyLogout(View):
+    template_name = 'logout.html'
 
-def signup(request):
-    if request.method == 'GET':
-        temp = loader.get_template('signup.html')
-        context = RequestContext(request, {
-                'session': request.session,
-        })
-        return HttpResponse(temp.render(context))
-    elif request.method == 'POST':
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect('/')
+
+class Signup(View):
+    template_name =  'signup.html'
+    
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
@@ -70,80 +63,80 @@ def signup(request):
         send_mail("Wellcome %s" % user.get_full_name(),"Hello, dumbass!", 'example.online.reader@gmail.com',[email])
         return HttpResponseRedirect('/login')
 
-def books(request):
-    books = Book.objects.filter(user = request.user.id)
-    temp = loader.get_template('books.html')
-    context = RequestContext(request, {
-        'books': books,
-    })
-    return HttpResponse(temp.render(context))
+class Books(View):
+    template_name = 'books.html'
 
-def book_id(request, id_num):
-    book = Book.objects.get(id = int(id_num))
-    page = 1
-    temp = loader.get_template('book.html')
-    context = RequestContext(request, {
-          'book': book,
-          'page': '\n'.join(book.pages(page)),
-          'page_previous': 1,
-          'page_next': 2,
-    })
-    return HttpResponse(temp.render(context))
+    def get(self,request):
+        books = Book.objects.filter(user = request.user.id)
+        return render(request, self.template_name, {'books': books })
 
-def book_id_on_page(request, id_num, page_num):
-    book = Book.objects.get(id = int(id_num))
-    temp = loader.get_template('book.html')
-    page_num = int(page_num)
-    context = RequestContext(request, {
-          'book': book,
-          'page_previous': page_num - 1,         
-          'page_next': page_num + 1,
-          'page': '\n'.join(book.pages(page_num)),
-    })
-    return HttpResponse(temp.render(context))
 
-def quotes(request):
-    quotes = Quote.objects.filter(user = request.user.id)
-    temp = loader.get_template('quotes.html')
-    context = RequestContext(request, {
-        'quotes': quotes,
-    })
-    return HttpResponse(temp.render(context))
+class BookId(View):
+    template_name = 'book.html'
 
-def quote(request):
-    user = User.objects.get(id = request.user.id)
-    book = Book.objects.get(id = request.POST['book_id'])
-    text = request.POST['quote_text']
-    quote = Quote(user = user, book = book, text = text) 
-    quote.save()
-    response = {'success': True}
-    return HttpResponse(json.dumps(response), content_type="application/json") 
+    def get(self, request, *args, **kwargs):
+        book = Book.objects.get(**kwargs)
+        page = 1
+        return render(request, self.template_name,{ 'book': book,
+                                                    'page': '\n'.join(book.pages(page)),
+                                                    'page_previous': 1,
+                                                    'page_next': 2,
+                                                  }) 
+class BookIdOnPage(View):
+    template_name = 'book.html'
 
-def reviews(request):
-    reviews = Review.objects.filter(user = request.user.id)
-    temp = loader.get_template('reviews.html')
-    context = RequestContext(request, {
-            'reviews': reviews,
-    })
-    return HttpResponse(temp.render(context))
+    def get(self,request, *args, **kwargs):
+        book = Book.objects.get(id = kwargs['id'])
+        page_num = int(kwargs['page_num'])
+        return render(request, self.template_name,{ 'book': book,
+                                                    'page_previous': page_num - 1,         
+                                                    'page_next': page_num + 1,
+                                                    'page': '\n'.join(book.pages(page_num)),
+                                                  })
 
-def review(request):
-    user = User.objects.get(id = request.user.id)
-    book = Book.objects.get(id = request.POST['book_id'])
-    text = request.POST['text']
-    review = Review(user = user, book = book, text = text)
-    review.save()
-    response = {'success': True }
-    return HttpResponse(json.dumps(response), content_type='application/json')    
+class MyQuotes(View):
+    template_name = 'quotes.html'
+    
+    def get(self, request):
+        quotes = Quote.objects.filter(user = request.user.id)
+        return render(request, self.template_name, {'quotes': quotes })
+
+class MyQuote(View):
+    
+    def post(self, request):
+        user = User.objects.get(id = request.user.id)
+        book = Book.objects.get(id = request.POST['book_id'])
+        text = request.POST['quote_text']
+        quote = Quote(user = user, book = book, text = text) 
+        quote.save()
+        response = {'success': True}
+        return HttpResponse(json.dumps(response), content_type="application/json") 
+
+class MyReviews(View):
+    template_name = 'reviews.html'
+
+    def get(self, request):
+        reviews = Review.objects.filter(user = request.user.id)
+        return render(request, self.template_name, { 'reviews': reviews })
+
+class MyReview(View):
+    
+    def post(self, request):
+        user = User.objects.get(id = request.user.id)
+        book = Book.objects.get(id = request.POST['book_id'])
+        text = request.POST['text']
+        review = Review(user = user, book = book, text = text)
+        review.save()
+        response = {'success': True }
+        return HttpResponse(json.dumps(response), content_type='application/json')    
      
-def upload(request):
-    if request.method == 'GET':
-        temp = loader.get_template('upload.html')
-        context = RequestContext(request, {
-                'session': request.session,
-        })
-        return HttpResponse(temp.render(context))
-    elif request.method == 'POST':
+class Upload(View):
+    template_name = 'upload.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
         text = request.FILES['book'].read()
         user = User.objects.get(id = request.user.id)
         book = Book(user = user, text = text, author = request.POST['author'], title = request.POST['title'])
--- booksite/urls.py
+++ booksite/urls.py
@@ -1,22 +1,22 @@
 from django.conf.urls import patterns, include, url
-
+from books.views import *
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('books.views',
-    url(r'^$', 'index'),
-    url(r'^login', 'my_login'),
-    url(r'^logout', 'my_logout'),
-    url(r'^signup', 'signup'),
-    url(r'^books', 'books'),
-    url(r'^book/(?P<id_num>\d+)/$', 'book_id'),
-    url(r'^book/(?P<id_num>\d+)/(?P<page_num>\d+)/$', 'book_id_on_page'),
-    url(r'^quotes', 'quotes'),
-    url(r'^quote', 'quote'),
-    url(r'^reviews', 'reviews'),
-    url(r'^review', 'review'),
-    url(r'^upload', 'upload'),
+urlpatterns = patterns('',
+    (r'^$', Index.as_view()),
+    (r'^login', MyLogin.as_view()),
+    (r'^logout', MyLogout.as_view()),
+    (r'^signup', Signup.as_view()),
+    (r'^books', Books.as_view()),
+    (r'^book/(?P<id>\d+)/$', BookId.as_view()),
+    (r'^book/(?P<id>\d+)/(?P<page_num>\d+)/$', BookIdOnPage.as_view()),
+    (r'^quotes', MyQuotes.as_view()),
+    (r'^quote', MyQuote.as_view()),
+    (r'^reviews', MyReviews.as_view()),
+    (r'^review', MyReview.as_view()),
+    (r'^upload', Upload.as_view()),
     # Examples:
     # url(r'^$', 'booksite.views.home', name='home'),
     # url(r'^booksite/', include('booksite.foo.urls')),
